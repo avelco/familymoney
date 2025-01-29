f690c79a813169d473d5f5d6d21fbb46a681f73a
Alejandro Velasco <alsoto2177@gmail.com>
2025-01-29 17:45:53 -0500

    avanzando =|

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let message = 'Welcome to Family Money!';
+</script>
+
+<main>
+	<h1>{message}</h1>
+	<p>This is the index page of your Svelte app.</p>
+</main>
--- src/routes/hooks.server.ts
+++ src/routes/hooks.server.ts
@@ -1,11 +1,12 @@
 import { redirect } from '@sveltejs/kit';
+import type { Handle } from '@sveltejs/kit';
 
-export const handle = async ({ event, resolve }) => {
+export const handle: Handle = async ({ event, resolve }) => {
 	// List of public routes that don't require authentication
 	const publicRoutes = ['/login', '/signup', '/'];
-
 	// Check if the current route is public
 	if (!publicRoutes.includes(event.url.pathname)) {
+		console.log(event.url.pathname);
 		// Check if the user is authenticated (e.g., by checking a cookie or token)
 		const userToken = event.cookies.get('userToken');
 
